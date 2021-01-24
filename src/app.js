cc424ab68f403ebdc898247a9efa92617d8e508b
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2021-01-24 18:36:18 +0000

    Tries to style corresponding latin and english words on hover (doesn't work!)

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+
+function hover() {
+  let latinConjunctionElement = document.querySelector("#latinConjunction");
+  let englishConjunctionElement = document.querySelector("#englishConjunction");
+
+  latinConjunctionElement.style.color = "blue";
+  englishConjunctionElement.style.color = "blue";
+
+}
