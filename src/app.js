f883bdc9f19cde55cb92fe374ac6ec40dd6ba0ae
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2021-01-28 17:26:37 +0000

    Adds link to William Whitaker's words for each Latin word.
    Very convoluted but it works. Would like this to show just the dictionary entry so that students have to look up the grammar. Or show what it "goes like" so students can look up grammar from the link. I initially tried this as an alert but it didn't work.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+let sauciaElement = document.querySelector("#saucia");
+
+sauciaElement.addEventListener("click", sauciaLookUp);
+
+function sauciaLookUp() {
+  alert("Hello");
+  console.log("test");
+}
+
+
+
