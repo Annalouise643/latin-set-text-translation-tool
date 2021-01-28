dc171c0990b6c66ebd9e95c659f5741ee43bc6e0
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2021-01-28 18:54:04 +0000

    Adds the hover feature for the rest of line one
    Need to find a cleaner way to do this!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function highlight() {
+function highlightRed() {
   let atElement = document.querySelector("#at");
   let butElement = document.querySelector("#but");
 
@@ -6,7 +6,7 @@ function highlight() {
   butElement.style.color = "red";
 }
 
-function undoHighlight() {
+function undoHighlightRed() {
   let atElement = document.querySelector("#at");
   let butElement = document.querySelector("#but");
 
@@ -14,7 +14,7 @@ function undoHighlight() {
   butElement.style.color = "black";
 }
 
-function highlightTwo() {
+function highlightPurple() {
   let reginaElement = document.querySelector("#regina");
   let queenElement = document.querySelector("#queen");
 
@@ -22,10 +22,77 @@ function highlightTwo() {
   queenElement.style.color = "purple";
 }
 
-function undoHighlightTwo() {
+function undoHighlightPurple() {
   let reginaElement = document.querySelector("#regina");
   let queenElement = document.querySelector("#queen");
 
   reginaElement.style.color = "black";
   queenElement.style.color = "black";
+}
+
+function highlightGreen() {
+  let graviElement = document.querySelector("#gravi");
+  let graveElement = document.querySelector("#grave");
+
+  graviElement.style.color = "green";
+  graveElement.style.color = "green";
+}
+
+function undoHighlightGreen() {
+  let graviElement = document.querySelector("#gravi");
+  let graveElement = document.querySelector("#grave");
+
+  graviElement.style.color = "black";
+  graveElement.style.color = "black";
+}
+function highlightBlue() {
+  let graviElement = document.querySelector("#iamdudum");
+  let graveElement = document.querySelector("#longTime");
+
+  graviElement.style.color = "blue";
+  graveElement.style.color = "blue";
+}
+
+function undoHighlightBlue() {
+  let graviElement = document.querySelector("#iamdudum");
+  let graveElement = document.querySelector("#longTime");
+
+  graviElement.style.color = "black";
+  graveElement.style.color = "black";
+}
+
+function highlightPaleBlue() {
+  let sauciaElement = document.querySelector("#saucia");
+  let inflictedElement = document.querySelector("#inflicted");
+
+  sauciaElement.style.color = "cornflowerblue";
+  inflictedElement.style.color = "cornflowerblue";
+}
+
+function undoHighlightPaleBlue() {
+  let sauciaElement = document.querySelector("#saucia");
+  let inflictedElement = document.querySelector("#inflicted");
+
+  sauciaElement.style.color = "black";
+  inflictedElement.style.color = "black";
+}
+
+function highlightOrange() {
+  let curaElement = document.querySelector("#cura");
+  let caresElement = document.querySelector("#cares");
+  let withElement = document.querySelector("#with");
+
+  curaElement.style.color = "orange";
+  caresElement.style.color = "orange";
+  withElement.style.color = "orange";
+}
+
+function undoHighlightOrange() {
+  let curaElement = document.querySelector("#cura");
+  let caresElement = document.querySelector("#cares");
+  let withElement = document.querySelector("#with");
+
+  curaElement.style.color = "black";
+  caresElement.style.color = "black";
+  withElement.style.color = "black";
 }
