5dca3192cfcacb194d5c4efe6455507d0522b62b
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2021-02-14 17:20:30 +0000

    Functionalise second line and remove html

--- src/app.js
+++ src/app.js
@@ -1,101 +1,4 @@
-function highlightRed() {
-  let atElement = document.querySelector("#at");
-  let butElement = document.querySelector("#but");
 
-  atElement.style.color = "red";
-  butElement.style.color = "red";
-}
-
-function undoHighlightRed() {
-  let atElement = document.querySelector("#at");
-  let butElement = document.querySelector("#but");
-
-  atElement.style.color = "black";
-  butElement.style.color = "black";
-}
-
-function highlightPurple() {
-  let reginaElement = document.querySelector("#regina");
-  let queenElement = document.querySelector("#queen");
-
-  reginaElement.style.color = "purple";
-  queenElement.style.color = "purple";
-}
-
-function undoHighlightPurple() {
-  let reginaElement = document.querySelector("#regina");
-  let queenElement = document.querySelector("#queen");
-
-  reginaElement.style.color = "black";
-  queenElement.style.color = "black";
-}
-
-function highlightGreen() {
-  let graviElement = document.querySelector("#gravi");
-  let graveElement = document.querySelector("#grave");
-
-  graviElement.style.color = "green";
-  graveElement.style.color = "green";
-}
-
-function undoHighlightGreen() {
-  let graviElement = document.querySelector("#gravi");
-  let graveElement = document.querySelector("#grave");
-
-  graviElement.style.color = "black";
-  graveElement.style.color = "black";
-}
-function highlightBlue() {
-  let graviElement = document.querySelector("#iamdudum");
-  let graveElement = document.querySelector("#longTime");
-
-  graviElement.style.color = "blue";
-  graveElement.style.color = "blue";
-}
-
-function undoHighlightBlue() {
-  let graviElement = document.querySelector("#iamdudum");
-  let graveElement = document.querySelector("#longTime");
-
-  graviElement.style.color = "black";
-  graveElement.style.color = "black";
-}
-
-function highlightPaleBlue() {
-  let sauciaElement = document.querySelector("#saucia");
-  let inflictedElement = document.querySelector("#inflicted");
-
-  sauciaElement.style.color = "cornflowerblue";
-  inflictedElement.style.color = "cornflowerblue";
-}
-
-function undoHighlightPaleBlue() {
-  let sauciaElement = document.querySelector("#saucia");
-  let inflictedElement = document.querySelector("#inflicted");
-
-  sauciaElement.style.color = "black";
-  inflictedElement.style.color = "black";
-}
-
-function highlightOrange() {
-  let curaElement = document.querySelector("#cura");
-  let anxietyElement = document.querySelector("#anxiety");
-  let withElement = document.querySelector("#with");
-
-  curaElement.style.color = "orange";
-  anxietyElement.style.color = "orange";
-  withElement.style.color = "orange";
-}
-
-function undoHighlightOrange() {
-  let curaElement = document.querySelector("#cura");
-  let anxietyElement = document.querySelector("#anxiety");
-  let withElement = document.querySelector("#with");
-
-  curaElement.style.color = "black";
-  anxietyElement.style.color = "black";
-  withElement.style.color = "black";
-}
 
 function highlight(lineIdx, wordIdxs, color) {
   wordIdxs.forEach(function (wordIdx) {
@@ -125,7 +28,7 @@ function renderTransWord(trans, lineIdx) {
   let el = document.createElement("span");
   el.innerHTML = trans.phrase;
   trans.sourceIdxs.forEach(function (sourceIdx) {
-    el.className += ` source-${lineIdx}-${sourceIdx}`;
+    el.className += `source-${lineIdx}-${sourceIdx}`;
   });
   el.addEventListener("mouseover", function () {
     highlight(lineIdx, trans.sourceIdxs, "red");
@@ -211,6 +114,20 @@ let data = {
         { phrase: "with", sourceIdxs: [5] },
         { phrase: "grave", sourceIdxs: [2] },
         { phrase: "anxiety", sourceIdxs: [5] },
+      ],
+    },
+
+    {
+      source: ["vulnus", "alit", "venis", "et", "caeco", "carpitur", "igni."],
+      trans: [
+        {phrase: "nourished", sourceIdxs: [1]},
+        {phrase: "a wound", sourceIdxs: [0]},
+        {phrase: "in her veins", sourceIdxs: [2]},
+        {phrase: "and", sourceIdxs: [3]},
+        {phrase: "was consumed", sourceIdxs: [5]},
+        {phrase: "by a", sourceIdxs: [6]},
+        {phrase: "hidden", sourceIdxs: [4]},
+        {phrase: "fire.", sourceIdxs: [6]}
       ],
     },
   ],
